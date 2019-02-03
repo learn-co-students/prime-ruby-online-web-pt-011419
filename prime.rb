9838ae76af6de544ad62c842e405894b4f6ca1a5
Ryan Mui <reachryanmui@gmail.com>
2019-02-03 18:11:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,5 @@
+# require 'benchmark'
+
 def prime?(int)
   if int > 1
     if (2..int).to_a.any? {|num| int % num == 0 && int != num}
@@ -8,3 +10,12 @@ def prime?(int)
   end
   return false
 end
+
+# iterations = 100_000
+# Benchmark.bmbm do |bm|
+#   bm.report('prime?') do
+#     iterations.times do
+#       prime?(1000)
+#     end
+#   end
+# end
