c21c547b03dafab2d0cb7016f18d736e51238aac
Ryan Mui <reachryanmui@gmail.com>
2019-02-03 17:59:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(int)
+  if int > 1
+    if (2..int).to_a.any? {|num| int % num == 0 && int != num}
+      return false
+    else
+      return true
+    end
+  end
+  return false
+end
