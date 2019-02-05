ce90d1e13ac0c27654dd852374f1ae2ba7535d84
David Acosta <davidacosta25081@yahoo.com>
2019-02-05 05:14:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+
+def prime?(integer)
+  number = 1
+  counter = 0
+  while number < integer
+    if integer % number == 0
+      counter += 1
+    end
+    number += 1
+  end
+  if counter == 2 return true
+  else
+    return false
+  end
+end
